86e9530dd8352e7a7a3cc836d0640ecced6e1b60
stonecrafter <kirien.chen@gmail.com>
2018-09-14 22:22:01 -0700

    Add cities and legend to world map

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Zoom from 'react-reveal/Zoom';
 import _ from 'lodash';
-import { ComposableMap, ZoomableGroup, Geographies, Geography } from 'react-simple-maps';
+import { ComposableMap, ZoomableGroup, Geographies, Geography, Markers, Marker } from 'react-simple-maps';
 import tooltip from 'wsdm-tooltip';
 
 import worldMapObject from './static/world-50m.json';
@@ -12,6 +12,14 @@ class WorldMap extends Component {
 
     this.countriesLived = ['CAN', 'USA', 'AUS', 'CHN'];
     this.countriesVisited = ['FRA', 'MEX', 'SWE', 'NOR', 'ISL', 'DNK'];
+    this.citiesLived = [
+      { name: 'Vancouver', coordinates: [-123.120735, 49.282730] },
+      { name: 'Toronto', coordinates: [-79.383186, 43.653225] },
+      { name: 'Sydney', coordinates: [151.205338, -33.872761] },
+      { name: 'Beijing', coordinates: [116.386340, 39.930840] },
+      { name: 'San Francisco', coordinates: [-122.419418, 37.774929] },
+      { name: 'Portland', coordinates: [-122.681430, 45.516020] }
+    ];
   }
 
   /**
@@ -36,7 +44,10 @@ class WorldMap extends Component {
     this.tip.create();
   }
 
-  handleMouseMove = (geography, evt) => {
+  /**
+   * Show tooltip on hover over a country
+   */
+  showCountryToolTip = (geography, evt) => {
     if (this.isImportantCountry(geography.id)) {
       this.tip.show(`
         <div class="tooltip-inner">
@@ -47,7 +58,19 @@ class WorldMap extends Component {
     }
   }
 
-  handleMouseLeave = () => {
+  /**
+   * Show tooltip on hover over a city
+   */
+  showCityToolTip = (marker, evt) => {
+    this.tip.show(`
+      <div class="tooltip-inner">
+        ${marker.name}
+      </div>
+    `);
+    this.tip.position({ pageX: evt.pageX, pageY: evt.pageY });
+  }
+
+  hideToolTip = () => {
     this.tip.hide();
   }
 
@@ -114,8 +137,8 @@ class WorldMap extends Component {
                     cacheId={ `geography-${i}` }
                     geography={ geography }
                     projection={ projection }
-                    onMouseMove={ this.handleMouseMove }
-                    onMouseLeave={ this.handleMouseLeave }
+                    onMouseMove={ this.showCountryToolTip }
+                    onMouseLeave={ this.hideToolTip }
                     style={{
                       // Colour behaviour should be the same for all 3 states
                       default: {
@@ -137,9 +160,47 @@ class WorldMap extends Component {
                   />
                 ))}
               </Geographies>
+
+              <Markers>
+                {this.citiesLived.map((city, i) => (
+                  <Marker
+                    className="world-map__marker"
+                    tabable={false}
+                    key={i}
+                    marker={city}
+                    onMouseMove={ this.showCityToolTip }
+                    onMouseLeave={ this.hideToolTip }
+                    style={{
+                      default: { fill: "FB97C6", stroke: "#62002E", outline: "none" },
+                      hover: { fill: "FB97C6", stroke: "#62002E", outline: "none" },
+                      pressed: { fill: "FB97C6", stroke: "#62002E", outline: "none" },
+                    }}
+                    >
+                    <g transform="translate(-12, -12) scale(2)">
+                      <path d="M5.019302312642594,0.4654667096328641 l1.3511287981041842,2.502725414919123 l2.895275995937537,0.4460302719657852 c0.2959615462513927,0.049558919107309536 0.373168906143061,0.39647135285847546 0.1672826130986133,0.545148110180404 l-2.058862930444471,2.0071362238460293 l0.45037626603472886,2.7876891997861515 c0.038603679945833934,0.2849637848670292 -0.2830936529361143,0.4708097315194396 -0.4889799459805628,0.3221329741975114 l-2.6250502363167003,-1.2637524372363889 l-2.6121823430014217,1.2761421670132163 c-0.27022575962083684,0.1238972977682736 -0.5404515192416738,-0.1115075679914461 -0.463244159350006,-0.34691243375116637 l0.43750837271945137,-2.800078929562979 l-2.058862930444471,-1.9947464940692021 c-0.21875418635972546,-0.2106254062060649 -0.06433946657638986,-0.5327583804035768 0.19301839972916973,-0.5327583804035768 l2.895275995937537,-0.4708097315194396 l1.338260904788906,-2.502725414919123 c0.12867893315277942,-0.2601843253133743 0.5018478392958406,-0.22301513598289277 0.5790551991875076,0.024779459553654758 z"/>
+                    </g>
+                  </Marker>
+                ))}
+              </Markers>
+
             </ZoomableGroup>
           </ComposableMap>
         </Zoom>
+
+        <div className="world-map__legend">
+          <div className="world-map__legend--cities">
+            <span></span>
+            <label>cities lived</label>
+          </div>
+          <div className="world-map__legend--visited">
+            <span></span>
+            <label>countries visited</label>
+          </div>
+          <div className="world-map__legend--lived">
+            <span></span>
+            <label>countries lived</label>
+          </div>
+        </div>
       </div>
     );
   }
