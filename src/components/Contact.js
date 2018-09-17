889afbdf4cdc3457fec2d6f8e109d8d611df4c07
stonecrafter <kirien.chen@gmail.com>
2018-09-16 22:45:47 -0700

    Contact form basics, add social icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+
+import GithubIcon from './icons/GithubIcon';
+import LinkedinIcon from './icons/LinkedinIcon';
+import CodepenIcon from './icons/CodepenIcon';
 
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <section className="contact" id="contact">
@@ -14,6 +15,38 @@ class Contact extends Component {
             <h1>contact me</h1>
           </div>
         </Fade>
+
+        <div className="contact__form">
+          <form action="https://formspree.io/stonewoodleaf@gmail.com" method="POST" autoComplete="off">
+            <Fade left>
+              <div className="contact__form--details">
+                <input type="text" name="name" id="nme" placeholder="Your Name" required />
+                <label htmlFor="nme" className="contact__form--label">Name</label>
+              </div>
+            </Fade>
+            <Fade right>
+              <div className="contact__form--details">
+                <input type="email" name="_replyto" id="eml" placeholder="Email" required />
+                <label htmlFor="eml" className="contact__form--label">Email</label>
+              </div>
+            </Fade>
+            <Fade bottom>
+              <div className="contact__form--message">
+                <textarea name="_replyto" id="msg" placeholder="Message" required ></textarea>
+                <label htmlFor="msg" className="contact__form--label">Message</label>
+              </div>
+            </Fade>
+            <Fade right><button type="submit" className="contact__form--submit">Send</button></Fade>
+          </form>
+        </div>
+
+        <Zoom>
+          <div className="contact__icons">
+            <a href="https://github.com/stonecrafter" target="_blank" rel="noopener noreferrer"><GithubIcon transform="scale(3)"/></a>
+            <a href="https://www.linkedin.com/in/stonecrafter" target="_blank" rel="noopener noreferrer"><LinkedinIcon transform="scale(3)"/></a>
+            <a href="https://codepen.io/stonecrafter" target="_blank" rel="noopener noreferrer"><CodepenIcon transform="scale(3)"/></a>
+          </div>
+        </Zoom>
       </section>
     );
   }
