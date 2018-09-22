45bc24598821da3ff6885dc8aac36ebf7bf2a56f
stonecrafter <kirien.chen@gmail.com>
2018-09-22 14:02:21 -0700

    Set up submission form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 
@@ -8,6 +9,26 @@ import LinkedinIcon from './icons/LinkedinIcon';
 import CodepenIcon from './icons/CodepenIcon';
 
 class Contact extends Component {
+  constructor() {
+    super();
+
+    // For form and submission
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      submitting: false,
+      submissionSuccess: undefined,
+      submissionMessage: ''
+    }
+  }
+
+  /**
+   * Update the state as form input changes
+   */
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
   /**
    * Submit the contact form
@@ -15,20 +36,34 @@ class Contact extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    const user = {
-      name: this.state.name
-    };
+    // Get the state values to be submitted
+    const { name, email, message } = this.state;
 
+    // Set the content type
     axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-    axios.post(`https://www.enformed.io/bpidqjmz/`, { user })
+    // Do the submission, reset any prior submission results
+    this.setState({ submitting: true, submissionMessage: '', submissionSuccess: undefined });
+    axios.post(`https://www.enformed.io/bpidqjmz/`, { name, email, message })
       .then(res => {
-        console.log(res);
-        console.log(res.data);
+        let submissionMessage, submissionSuccess;
+        // If the request failed, the status will still be 200 OK, but the URL will be /failure
+        // A weird way to check, but it works...
+        if (_.includes(res.request.responseURL, 'failure')) {
+          submissionMessage = 'Uh oh, there was an error! Try one of the methods below to get in touch instead...';
+          submissionSuccess = false;
+        } else {
+          submissionMessage = 'Sent! I will get back to you shortly...';
+          submissionSuccess = true;
+        }
+
+        this.setState({ submitting: false, submissionMessage, submissionSuccess });
       })
   }
 
   render() {
+    const { name, email, message } = this.state;
+
     return (
       <section className="contact" id="contact">
         <Fade bottom>
@@ -41,24 +76,31 @@ class Contact extends Component {
           <form onSubmit={this.handleSubmit} autoComplete="off">
             <Fade left>
               <div className="contact__form--details">
-                <input type="text" name="name" id="nme" placeholder="Your Name" required />
+                <input type="text" name="name" id="nme" value={name} onChange={this.onChange} placeholder="Your Name" required />
                 <label htmlFor="nme" className="contact__form--label">Name</label>
               </div>
             </Fade>
             <Fade right>
               <div className="contact__form--details">
-                <input type="email" name="_replyto" id="eml" placeholder="Email" required />
+                <input type="email" name="email" id="eml" value={email} onChange={this.onChange} placeholder="Email" required />
                 <label htmlFor="eml" className="contact__form--label">Email</label>
               </div>
             </Fade>
             <Fade bottom>
               <div className="contact__form--message">
-                <textarea name="_replyto" id="msg" placeholder="Message" required ></textarea>
+                <textarea name="message" id="msg" placeholder="Message" value={message} onChange={this.onChange} required ></textarea>
                 <label htmlFor="msg" className="contact__form--label">Message</label>
               </div>
             </Fade>
             <input type="hidden" name="*honeypot" />
-            <Fade right><button type="submit" className="contact__form--submit">Send</button></Fade>
+            <Fade right>
+              <button type="submit" className="contact__form--submit" disabled={this.state.submitting}>
+                {this.state.submitting ? 'Sending' : 'Send'}
+              </button>
+            </Fade>
+            <div className={'contact__form--result ' + this.state.submissionSuccess}>
+              {this.state.submissionMessage && <Fade>{this.state.submissionMessage}</Fade>}
+            </div>
           </form>
         </div>
 
